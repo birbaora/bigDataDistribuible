824be60334d6f89f5a2959c383519447448655cd
Nacho <birbaora@yahoo.es>
2024-10-14 19:56:24 +0000

    si

---
+++ preparaGIT-token.sh
@@ -0,0 +1,7 @@
+git config --global user.name "Nacho"
+
+git config pull.rebase true
+
+git config --global user.email birbaora@yahoo.es
+
+git remote set-url origin https://github.com/birbaora/bigDataDistribuible.git
