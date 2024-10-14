ceb90e7362887fb4a8f367295e4b1a4a49c5b8e2
Nacho <birbaora@yahoo.es>
2024-10-14 20:16:15 +0000

    preparaGIT_token

--- preparaGIT-token.sh
+++ preparaGIT-token.sh
@@ -4,4 +4,8 @@ git config pull.rebase true
 
 git config --global user.email birbaora@yahoo.es
 
-git remote set-url origin https://github.com/birbaora/bigDataDistribuible.git
+#git remote set-url origin https://git@github.com/birbaora/bigDataDistribuible.git
+
+git remote set-url origin git@github.com:birbaora/bigDataDistribuible.git
+
+git config --global url."git@github.com:".insteadOf "https://github.com/"
