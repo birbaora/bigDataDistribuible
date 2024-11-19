1109f12fcf95764a3e7e477c45ed5d33a06d06ca
Nacho <birbaora@yahoo.es>
2024-11-19 10:39:51 +0100

    tests

--- preparaGIT-token-github.sh
+++ preparaGIT-token-github.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-################
 git config --global user.name "Nacho"
 
 git config pull.rebase true
@@ -18,20 +15,3 @@ git remote set-url origin git@github.com:birbaora/bigDataDistribuible.git
 
 git config --global url."git@github.com:".insteadOf "https://github.com/"
 git config --global url."git://".insteadOf "https://"
-#########################
-
-original=no
- if [ $original == "si" ] ; then
-
-	 git config --global user.name "Nacho"
-
-	 git config pull.rebase true
-
-	 git config --global user.email birbaora@yahoo.es
-
-	 #git remote set-url origin https://git@github.com/birbaora/bigDataDistribuible.git
-
-	 git remote set-url origin git@github.com:birbaora/bigDataDistribuible.git
-
-	 git config --global url."git@github.com:".insteadOf "https://github.com/"
-fi
